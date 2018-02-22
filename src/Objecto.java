48000bd3674697228815950688c667eea55a6949
Ruben1996 <rubencobe96@gmail.com>
2018-02-22 11:23:54 +0100

    Clases Acabadas

--- src/Objecto.java
+++ src/Objecto.java
@@ -1,10 +1,58 @@
 public class Objecto {
 
-    String Nombre;
-    String Tipo;
-    String Descripcion;
-    int Valor;
-    int Coste;
+    private String Nombre;
+    private String Tipo;
+    private String Descripcion;
+    private int Valor;
+    private int Coste;
 
+    public double getNom() {
 
+        return Nombre;
+    }
+
+    public void setNom(String Nom) {
+
+        this.Nombre = Nom;
+    }
+
+    public double getTipo() {
+
+        return Tipo;
+    }
+
+    public void setTipo(String Tip) {
+
+        this.Tipo = Tip;
+    }
+
+    public double getDescripcion() {
+
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Des) {
+
+        this.Descripcion = Des;
+    }
+
+    public double getValor() {
+
+        return Valor;
+    }
+
+    public void setValor(int valor) {
+
+        this.Valor = valor;
+    }
+
+    public double getCoste() {
+
+        return Coste;
+    }
+
+    public void setCoste(int coste) {
+
+        this.Coste = coste;
+    }
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,13 +1,71 @@
 public class Usuario {
 
-    String Nombre;
-    String Password;
-    int Vida;
-    int Ataque;
-    int Defensa;
-    int Resistencia;
+    private String Nombre;
+    private String Password;
+    private int Vida;
+    private int Ataque;
+    private int Defensa;
+    private int Resistencia;
 
+    public double getNom() {
 
+        return Nombre;
+    }
+
+    public void setNom(String Nom) {
+
+        this.Nombre = Nom;
+    }
+
+    public double getPass() {
+
+        return Password;
+    }
+
+    public void setPass(String Pass) {
+
+        this.Password = Pass;
+    }
+
+    public double getVida() {
+
+        return Vida;
+    }
+
+    public void setVida(int vida) {
+
+        this.Vida = vida;
+    }
+
+    public double getAtaque() {
+
+        return Ataque;
+    }
+
+    public void setAtaque(int atac) {
+
+        this.Ataque = atac;
+    }
+
+    public double getDefensa() {
+
+        return Defensa;
+    }
+
+    public void setDefensa(int Defen) {
+
+        this.Defensa = Defen;
+    }
+
+    public double getResistencia() {
+
+        return Resistencia;
+    }
+
+    public void setResistencia(int Resis) {
+
+        this.Resistencia = Resis;
+    }
 
 }
 
